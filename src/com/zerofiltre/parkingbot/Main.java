14ce46e6f85fa47b5af3bb3abb8d50a446d566b2
rino <severin.komenan@sotels.ci>
2024-12-18 16:07:49 +0000

    premier commit du projet

--- src/com/zerofiltre/parkingbot/Main.java
+++ src/com/zerofiltre/parkingbot/Main.java
@@ -7,7 +7,18 @@ public class Main {
        int length = 180;
        int width = 120;
        int surface = length * width;
+
+       int perimeter = length + width * 2 ;
+
+       int difference = length - width ;
+
+
+
+       double ratio = (double) width / length;
        System.out.println("La surface au sol de la voiture est de: "+ surface);
+       System.out.println("Le perimetre  de la voiture  est de: "+ perimeter);
+       System.out.println("la difference   est de: "+ difference);
+       System.out.println("le ratio est de: "+ ratio);
 
     }
 }
---
+++ src/com/zerofiltre/parkingbot/algo1.java
@@ -0,0 +1,14 @@
+package com.zerofiltre.parkingbot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class algo1 {
+
+
+    // verifier dans la liste des nombre suivant s'ils sont impaire ou impaire
+
+    List<Integer>nombres = new ArrayList<>();
+
+
+}
